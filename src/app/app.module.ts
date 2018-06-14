47dba39e2adb9a0e3c5aebeeac9d1aafe943b951
Don Lazov <DLazov@follett.com>
2018-06-14 17:06:40 -0400

    Added Material Design to the project.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,9 @@
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 
+import { MyMaterialModule } from './material.module';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ConfigComponent } from './config/config.component';
@@ -21,7 +23,7 @@ import { MessagesComponent } from './messages/messages.component';
     PhaserDetailComponent,
     MessagesComponent
   ],
-  imports: [BrowserModule, AppRoutingModule, FormsModule],
+  imports: [BrowserModule, BrowserAnimationsModule, AppRoutingModule, FormsModule, MyMaterialModule],
   providers: [
     // no need to place any providers due to the `providedIn` flag...
   ],
